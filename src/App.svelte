cdf37501fc3e9be73ab1e13018f06aad79bf45d5
Muchlis Rachman <muchlizz@gmail.com>
2020-09-14 12:42:03 +0700

    Add css

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
-<h1>Charity</h1>
+<script>
+    let title = "Charity";
+    setTimeout(function(){
+        title = "Donation";
+    },2000);
+</script>
+
+<h1>{title}</h1>
+
+<style>
+h1 {
+    font-size: 3em;
+    color: #e2071c;
+}
+</style>
