2a1218440d3396c82a8d78de9f09cb0488ed98a6
Muchlis Rachman <muchlizz@gmail.com>
2020-09-24 16:02:44 +0700

    Bahasa Template Svelte

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -4,11 +4,10 @@
 <div>
     <h2>Daftar Charity</h2>
     {#if charities !== undefined}
-        <pre>{charities}</pre>
         <ul>
-            <li>Charity 1</li>
-            <li>Charity 2</li>
-            <li>Charity 3</li>
+            {#each charities as char }
+                <li>{char}</li>
+            {/each}
         </ul>
     {:else}
         <h5>Data Belum Tersedia</h5>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,7 +2,7 @@
     import CharityList from '../components/CharityList.svelte';
     
     let title = "Charity";
-    let charities = ['Charity 1', 'Charity 2', 'Charity 3'];
+    let charities = ["Charity 1", "Charity 2", "Charity 3", "Charity 4"];
 </script>
 
 <style>
