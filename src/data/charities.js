b5b859707c18cc0ae659283341816be95ff60792
Muchlis Rachman <muchlizz@gmail.com>
2020-10-08 00:38:54 +0700

    HTML Template Ke Komponen Svelte

--- src/data/charities.js
+++ src/data/charities.js
@@ -5,9 +5,13 @@ module.exports  = {
             title: 'First Charity Project',
             category: 'Money',
             thumbnail: 'https://live.staticflickr.com/5728/31412895946_104b4d9379_w.jpg',
-            pledged: 0,
+            pledged: 99000,
             target: 100000,
-            date_end: new Date('10 August 2020')
+            date_end: +new Date('10 December 2020'),
+            profile_photo: 'https://live.staticflickr.com/7637/16798720551_eaf3411ee6.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledgeds: 0
+
         }
     ]
 }
